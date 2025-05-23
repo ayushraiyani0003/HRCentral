fa8d4dc72119d3bd3696b333fac8c1ccad1e0b79
Ayushraiyani0003 <ayushraiyani03@gmail.com>
2025-05-23 13:32:51 +0100

    remove : all model and database tables relented things

--- server/models/employeeModels/index.js
+++
@@ -1,9 +0,0 @@
-const { employeeDb } = require("../../config/db");
-
-// Import Employee model
-const Employee = require("./employee")(employeeDb);
-
-// Export the Employee model
-module.exports = {
-    Employee,
-};
