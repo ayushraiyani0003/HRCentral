4856733d0f5a8c2662337cf1a43cbe9ddbe923c7
Ayushraiyani0003 <ayushraiyani03@gmail.com>
2025-05-23 13:31:58 +0100

    edit: button click and open correct model

--- client/src/features/employee/components/index.js
+++ client/src/features/employee/components/index.js
@@ -1,4 +1,8 @@
 import EmployeesTabs from './Tabs/EmployeesTabs/EmployeesTabs'
 import WorkHistoryTabs from './Tabs/WorkHistoryTabs/WorkHistoryTabs'
 
-export { EmployeesTabs, WorkHistoryTabs }
+
+// hear defile all models 
+import EmployeePageModels from './models/EmployeePageModels'
+
+export { EmployeesTabs, WorkHistoryTabs, EmployeePageModels }
